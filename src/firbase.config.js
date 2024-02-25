88b8d5ebf8cb76cfb2939a6294e6d4fc12ad4ae6
sunnyDas101 <sunnyofficial101@gmail.com>
2024-02-25 13:05:30 +0530

    new file created

---
+++ src/firbase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdB0FnWC0i8dFQsOGzbjejKo9sBO6F6gk",
+  authDomain: "figmadesigndevelopment.firebaseapp.com",
+  projectId: "figmadesigndevelopment",
+  storageBucket: "figmadesigndevelopment.appspot.com",
+  messagingSenderId: "461448576833",
+  appId: "1:461448576833:web:46300545aa3645dbee2bc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
